63eabfa8130bb225c5c2cdf5bf6810eef488ca06
Gan Jun Kai <kuhn96@gmail.com>
2015-10-18 09:19:33 +0800

    Let the connection close automatically

--- src/com/carrental/controller/manager/Facade.java
+++ src/com/carrental/controller/manager/Facade.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
  * @version 1.0
  * @since 1.0
  */
-public class Facade {
+public class Facade implements AutoCloseable {
 	private Connection connection;
 	
 	public Facade() throws ClassNotFoundException, SQLException {
